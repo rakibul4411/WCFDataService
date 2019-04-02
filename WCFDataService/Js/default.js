2b5aca7a4e4e194fdd8155a75f6c157044a476fc
rakibul4411 <rakibul4411@gmail.com>
2019-04-02 18:31:26 +0600

    First commit

---
+++ WCFDataService/Js/default.js
@@ -0,0 +1,25 @@
+﻿var myApp = angular.module("myApp", []);
+myApp.controller("mainController", function ($scope, $http) {
+    $scope.PersonVM = {};
+    function GetAll() {
+        $http({
+            method: "GET",
+            url: "http://localhost:55282/WcfDataService1.svc/People",
+            dataType: "json",
+        }).then(
+
+            function (response) {
+               // $scope.PersonVM = response.data;
+                $.each(response.data, function (key, value) {
+                    $scope.PersonVM = value;                    
+                })   
+                console.log($scope.PersonVM);
+            },
+            function () {
+                alert("Failed.");
+                console.log(response.data);
+            });
+    }
+    GetAll();
+    console.log($scope.PersonVM);
+});
